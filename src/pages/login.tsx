2872c79d21f73c976d4645affea81662cc0b661a
Juan Diego Quijada <jdquijada.22@est.ucab.edu.ve>
2024-05-19 22:46:21 -0400

    probando componentes

---
+++ src/pages/login.tsx
@@ -0,0 +1,33 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButton, IonFooter, IonText } from '@ionic/react';
+import './login.css';
+
+const Login: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Iniciar sesión</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+            <IonTitle size="large">Iniciar sesión</IonTitle>
+        </IonHeader>
+        <IonList>
+
+          <IonItem>
+           <IonInput label="Usuario"></IonInput>
+          </IonItem>
+
+          <IonItem>
+           <IonInput label="Contraseña" type="password"></IonInput>
+          </IonItem>
+
+        </IonList>
+        <IonButton>Acceder</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
