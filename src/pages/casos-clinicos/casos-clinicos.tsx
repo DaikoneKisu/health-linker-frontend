9e4b1be318167cf5e14d6a2c2d575df2715bf5a5
Juan Diego Quijada <jdquijada.22@est.ucab.edu.ve>
2024-05-23 20:24:12 -0400

    añadido el diseño de módulo de casos clínicos

--- src/pages/casos-clinicos/casos-clinicos.tsx
+++ src/pages/casos-clinicos/casos-clinicos.tsx
@@ -3,29 +3,111 @@ import {
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar,
-  IonList,
-  IonItem,
-  IonInput,
+  IonCard,
+  IonButtons,
   IonButton,
-  IonFooter,
+  IonMenuButton,
   IonText,
+  IonList,
+  IonSearchbar,
+  IonFooter,
+  IonIcon,
 } from "@ionic/react";
+import {home, chatbox, folder, mail, person} from 'ionicons/icons';
 import "./styles.css";
 
 const CasosClinicos: React.FC = () => {
   return (
-    <IonPage style={{ backgroundColor: "blue" }}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          backgroundColor: "blue",
-        }}
-      >
-        <h1 style={{ color: "green" }}>Casos Clínicos</h1>
-      </div>
+    <IonPage>
+      <IonHeader>
+          <IonTitle className='titulo-app'>Casos clínicos</IonTitle>
+          <IonButtons>
+            <IonButton className="botones-casos">Cerrados</IonButton>
+            <IonButton className="botones-casos">Abiertos</IonButton>
+          </IonButtons>
+      </IonHeader>
+
+      <IonSearchbar></IonSearchbar>
+
+
+      <IonContent>
+        <IonList>
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+
+          <IonCard>
+            <IonText>
+              <h2>Gabriel Cordoba</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis sequi cumque temporibus, delectus minus culpa quos velit magni iste ducimus ipsam quidem praesentium, quia architecto voluptatem exercitationem, quo facere!</p>
+            </IonText>
+          </IonCard>
+        </IonList>
+      </IonContent>
+
+      <IonFooter>
+        <IonButtons>
+
+          <IonButton size="large" className="botones-inferiores">
+            <IonIcon icon={home}></IonIcon>
+          </IonButton>
+          
+          <IonButton size="large" className="botones-inferiores">
+            <IonIcon icon={chatbox}></IonIcon>
+          </IonButton>
+
+          <IonButton size="large" className="botones-inferiores">
+            <IonIcon icon={folder}></IonIcon>
+          </IonButton>
+
+          <IonButton size="large" className="botones-inferiores">
+            <IonIcon icon={mail}></IonIcon>
+          </IonButton>
+
+          <IonButton size="large" className="botones-inferiores">
+            <IonIcon icon={person}></IonIcon>
+          </IonButton>
+
+        </IonButtons>
+      </IonFooter>
     </IonPage>
   );
 };
