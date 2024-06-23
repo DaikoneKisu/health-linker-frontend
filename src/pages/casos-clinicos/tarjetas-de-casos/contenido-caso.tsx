36b8fdd662d1303fd20b62b8d3bcd4e648b3651e
GreenieWarren <87400557+GreenieWarren@users.noreply.github.com>
2024-06-23 02:38:21 -0400

    Agregando personalización al caso

--- src/pages/casos-clinicos/tarjetas-de-casos/contenido-caso.tsx
+++ src/pages/casos-clinicos/tarjetas-de-casos/contenido-caso.tsx
@@ -16,7 +16,7 @@ import {
   IonItem,
 } from "@ionic/react";
 import { CasoClinico } from "../types";
-import { add, pencil, trash } from "ionicons/icons";
+import { person } from "ionicons/icons";
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
@@ -30,10 +30,33 @@ export const CasoIndividual: React.FC<Props> = ({
         <IonTitle className="tittle-style">Health-Linker</IonTitle>
         <IonText className="subtittle-style">Casos Clínicos</IonText>
       </IonHeader>
-
       <IonButton className="botones-caso" onClick={() => dentroCaso(false)}>
         boton de regreso
       </IonButton>
+      <IonIcon
+        icon={person}
+        style={{ width: 128, height: 128, paddingTop: 10, paddingBottom: 10 }}
+      ></IonIcon>
+
+      <div style={{ padding: 10 }}></div>
+      <div className="init-div-style">
+        <IonText className="text-style">
+          Fecha de nacimiento: {casoClinico.fechaNacimiento}
+        </IonText>
+        <IonText className="text-style">Genero: {casoClinico.genero}</IonText>
+        <IonText className="text-style">
+          Especialidad requerida: {casoClinico.especialidadRequerida}
+        </IonText>
+        <IonText className="text-style">
+          Motivo de la mentoría: {casoClinico.motivoMentoria}
+        </IonText>
+      </div>
+
+      <IonList>
+        <IonCard>
+          <IonText>testing bro</IonText>
+        </IonCard>
+      </IonList>
 
       <IonFooter></IonFooter>
     </div>
