84f348070e42242e7211808225da2e368d8dd4e8
AndresChacon00 <andresroberto.c@gmail.com>
2024-11-11 20:04:37 -0400

    si

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,1 +1,2 @@
-export const SERVER = "https://healthlinker-backend-stage.saludremota.org";
+//export const SERVER = "https://healthlinker-backend-stage.saludremota.org";
+export const SERVER = "http://localhost:8080";
