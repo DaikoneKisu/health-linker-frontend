7747aba06bbded635024e23a720dd8e696e261cd
AndresChacon00 <andresroberto.c@gmail.com>
2024-11-23 19:25:48 -0400

    add imagenes recursos educativos

--- src/components/QuillEditor.tsx
+++ src/components/QuillEditor.tsx
@@ -3,6 +3,38 @@ import ReactQuill from 'react-quill';
 import { FieldProps } from 'formik';
 import 'react-quill/dist/quill.snow.css';
 
+// Opciones de la barra de herramientas
+const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image', 'video'],
+      ['color', 'background'],
+      ['clean'],
+      ['code-block']
+    ],
+  };
+  
+  // Formatos permitidos
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'video',
+    'color',
+    'background',
+    'code-block'
+  ];
+
 interface QuillEditorProps extends FieldProps {
     placeholder?: string;
 }
@@ -19,6 +51,8 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ field, form, placeholder}) =>
             onBlur={() => form.setFieldTouched(field.name, true)}
             theme="snow"
             placeholder={placeholder}
+            modules={modules}
+            formats={formats}
             style={{ height: '200px', marginBottom:'50px' }}
         />
     )
